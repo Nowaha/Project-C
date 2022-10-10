58a237a3eea74ca9038c86d22e4e19d2528aac4c
Noah <noahverkaik@gmail.com>
2022-10-10 11:29:02 +0200

    Android start + Login screen

---
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/LoginFragment.kt
@@ -0,0 +1,7 @@
+package xyz.nowaha.chengetawildlife
+
+import androidx.fragment.app.Fragment
+
+// Should contain the code for the logging in process.
+// After logging in, Session.key should be set to the session key retrieved.
+class LoginFragment : Fragment(R.layout.fragment_login)
