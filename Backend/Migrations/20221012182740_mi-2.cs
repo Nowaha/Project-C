9ad148b872247ae14946b5d29ddb9b4e2a04e973
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2022-10-12 20:58:28 +0200

    migrations + tables

---
+++ Backend/Migrations/20221012182740_mi-2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ChengetaBackend.Migrations
+{
+    public partial class mi2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Username = table.Column<string>(type: "varchar(24)", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Salt = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "session",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IdRanger = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_session", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "session");
+        }
+    }
+}
--- Backend/Model.cs
+++ Backend/Model.cs
@@ -1,38 +1,50 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ChengetaBackend
 {
-
-    public class ChengetaContext : DbContext {
-
+    public class ChengetaContext : DbContext
+    {
         public static string DB_PASSWORD = "123";
         public static int DB_PORT = 5432;
 
         public DbSet<Event> events { get; set; } = null!;
+        public DbSet<Account> accounts { get; set; } = null!;
+        public DbSet<Session> session { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            optionsBuilder.UseNpgsql($"User ID=postgres;Password={DB_PASSWORD};Host=localhost;port={DB_PORT};Database=ChengetaApp;Pooling=true");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                $"UserID=postgres;Password=eniddehalas;Host=localhost;port=5432;Database=ChengetaApp;Pooling=true"
+            );
         }
-        
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        //     modelBuilder.Entity<Session>().HasOne(a => a.Ranger).WithOne().HasForeignKey<Session>(b => b.Id);
+        // }  
     }
 
-    public class Event {
+    public class Event
+    {
         public int Id { get; set; }
-        
+
         [Required]
         public int NodeId { get; set; }
+
         [Required]
         public DateTime Date { get; set; }
 
         [Required]
         public float Latitude { get; set; }
+
         [Required]
         public float Longitude { get; set; }
 
         [Required]
         public string SoundLabel { get; set; }
+
         [Required]
         public int Probability { get; set; }
 
@@ -40,4 +52,55 @@ namespace ChengetaBackend
         public string SoundURL { get; set; }
     }
 
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(24)")]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+
+        [Required]
+        public AccountType Role { get; set; }
+
+        public enum AccountType
+        {
+            RANGER,
+            ADMIN
+        }
+    }
+
+    // public class statussen
+    // {
+    //     public int Id { get; set; }
+
+    //     [Required]
+    //     public int RangerId { get; set; }
+
+    //     [Required]
+    //     public int Status { get; set; }
+    // }
+
+    public class Session
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Start { get; set; }
+
+        [Required]
+        [ForeignKey("RangerId")]
+        public Account Ranger { get; set; }
+        
+        public int RangerId {get;set;}
+    }
 }
