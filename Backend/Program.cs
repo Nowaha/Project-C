5a4c5dc4e13f5ffc6e43339a6792062209467c8d
Nowaha <noahverkaik@gmail.com>
2022-10-15 22:47:39 +0200

    Test updates

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,26 +1,57 @@
 ﻿using System.Threading.Tasks;
 using MQTTnet;
+using System.Linq;
 
-namespace ChengetaBackend {
+namespace ChengetaBackend
+{
 
-    public class Program {
+    public class Program
+    {
 
         public static MqttFactory mqttFactory = new MqttFactory();
 
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
             runTests();
-            
-            Run().Wait();
+
+            var ses = new SessionManager();
+
+            using (var db = new ChengetaContext())
+            {
+                if (db.accounts.Where(a => a.Username == "admin").FirstOrDefault() == null)
+                {
+                    var newPassDetails = Utils.HashNewPassword("Pass123");
+                    var adminPassHash = newPassDetails.hashed;
+                    var adminSalt = newPassDetails.salt;
+
+                    db.accounts.Add(new Account()
+                    {
+                        CreationDate = System.DateTime.UtcNow,
+                        Username = "admin",
+                        Password = adminPassHash,
+                        Salt = adminSalt,
+                        Role = Account.AccountType.ADMIN
+                    });
+
+                    db.SaveChanges();
+                }
+            }
+
+            System.Console.WriteLine(ses.Authenticate("admin", "Pass123").sessionKey);
+
+            //Run().Wait();
         }
 
-        public static async Task Run() {
+        public static async Task Run()
+        {
             ChengetaBackend.MQTTClient client = new MQTTClient(mqttFactory);
             await client.Connect();
         }
 
-        private static void runTests() {
+        private static void runTests()
+        {
             AuthenticationTest.testHashSaltAndPasswordUniqueness();
-            AuthenticationTest.testSessionCreationOnlyWhenPasswordValid();
+            //AuthenticationTest.testSessionCreationOnlyWhenPasswordValid();
         }
 
     }
