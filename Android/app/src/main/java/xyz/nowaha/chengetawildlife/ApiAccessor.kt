585bf8dae32e42d03ff799b788f3302abf6ff202
Nowaha <noahverkaik@gmail.com>
2022-10-17 00:04:12 +0200

    Added API accessor, with login specific one

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/ApiAccessor.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/ApiAccessor.kt
@@ -1,37 +1,74 @@
 package xyz.nowaha.chengetawildlife
 
-import androidx.lifecycle.LifecycleCoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLEncoder
-import java.util.concurrent.Future
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import java.io.BufferedInputStream
+import java.net.*
 
 object ApiAccessor {
 
-    val urlBase = "http://192.168.178.175:34100"
+    private const val URL_BASE = "http://192.168.178.175:34100"
 
-    fun attemptLogin(username: String, password: String): String {
-        return sendGet("/user/login", Pair("username", username), Pair("password", password))
-    }
-
-    fun sendGet(path: String, vararg args: Pair<String, String>): String {
-        val url = URL(urlBase + path + "?" + args.joinToString("&") { URLEncoder.encode(it.first) + "=" + URLEncoder.encode(it.second) })
-
-        with(url.openConnection() as HttpURLConnection) {
-            requestMethod = "GET"
-            setRequestProperty("Authorization", Session.key ?: "NO_SESSION")
-            setRequestProperty("Accept", "text/html")
-
-            println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
-            inputStream.bufferedReader().use {
-                println(it.readLines().joinToString("\n"))
-            }
+    suspend fun attemptLogin(username: String, password: String): JsonObject? =
+        withContext(Dispatchers.IO) {
+            return@withContext sendGet(
+                "/user/login",
+                "username" to username,
+                "password" to password
+            )
         }
 
-        return "";
-    }
+    suspend fun sendGet(path: String, vararg args: Pair<String, String>): JsonObject? =
+        withContext(Dispatchers.IO) {
+            var connection: HttpURLConnection? = null
+
+            var attempts = 0
+
+            while (attempts < 5) {
+                try {
+                    kotlin.runCatching {
+                        val url = URL("$URL_BASE$path?" + args.joinToString("&") {
+                            URLEncoder.encode(it.first) + "=" + URLEncoder.encode(it.second)
+                        })
+
+                        connection = url.openConnection() as HttpURLConnection;
+                        with(connection as HttpURLConnection) {
+                            requestMethod = "GET"
+                            connectTimeout = 5000
+
+                            setRequestProperty("Authorization", Session.key ?: "NO_SESSION")
+                            setRequestProperty("Accept", "*")
+
+                            println("Sent 'GET' request to URL : $url; Response Code : $responseCode")
+
+                            connection!!.inputStream.bufferedReader().use {
+                                it.readText()
+                            }
+                        }
+                    }.onSuccess {
+                        return@withContext Json.decodeFromString(it)
+                    }.onFailure {
+                        if (connection == null || (connection as HttpURLConnection).responseCode == 404) {
+                            return@withContext null;
+                        }
+
+                        connection!!.errorStream.bufferedReader().use {
+                            return@withContext Json.decodeFromString(it.readText())
+                        }
+                    }
+                } catch (ex: ProtocolException) {
+                    ex.printStackTrace()
+                    attempts++
+                } catch (ex: SocketTimeoutException) {
+                    ex.printStackTrace()
+                    return@withContext null;
+                }
+            }
+
+            return@withContext null;
+        }
 
 }
