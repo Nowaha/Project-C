3116b711fd7ab21ea1e752eb5df4b8760df3b4c6
Noah <noahverkaik@gmail.com>
2022-10-17 15:59:43 +0200

    Increased max rows & modified date to use ms

--- Backend/requests/LatestEventsRequestHandler.cs
+++ Backend/requests/LatestEventsRequestHandler.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Tracing;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,7 +14,7 @@ namespace ChengetaBackend
 
         private static int minimumRowCount = 1;
         private static int defaultRowCount = 10;
-        private static int maximumRowCount = 50;
+        private static int maximumRowCount = 100;
 
         private static int minimumOffset = 0;
         private static int defaultOffset = 0;
@@ -22,36 +23,55 @@ namespace ChengetaBackend
         public Response HandleRequest(string session, Dictionary<string, string> args)
         {
             if (!Program.sessionManager.SessionDictionary.ContainsKey(session)) return Response.generateBasicError(Code.UNAUTHORIZED, Message.UNAUTHORIZED, "Invalid session");
-            
+
             int rows = defaultRowCount;
             int offset = defaultOffset;
 
-            if (args.ContainsKey("rows")) {
+            if (args.ContainsKey("rows"))
+            {
                 int rowsTemp;
-                if (int.TryParse(args["rows"], out rowsTemp)) {
+                if (int.TryParse(args["rows"], out rowsTemp))
+                {
                     if (rowsTemp < minimumRowCount) return Response.generateBasicError(Code.BAD_REQUEST, Message.BAD_REQUEST, $"Minimum row count of {minimumRowCount}.");
                     if (rowsTemp > maximumRowCount) return Response.generateBasicError(Code.BAD_REQUEST, Message.BAD_REQUEST, $"Maximum row count of {maximumRowCount}.");
                     rows = rowsTemp;
                 }
             }
 
-            if (args.ContainsKey("offset")) {
+            if (args.ContainsKey("offset"))
+            {
                 int offsetTemp;
-                if (int.TryParse(args["offset"], out offsetTemp)) {
+                if (int.TryParse(args["offset"], out offsetTemp))
+                {
                     if (offsetTemp < minimumOffset) return Response.generateBasicError(Code.BAD_REQUEST, Message.BAD_REQUEST, $"Minimum offset count of {minimumOffset}.");
                     if (offsetTemp > maximumOffset) return Response.generateBasicError(Code.BAD_REQUEST, Message.BAD_REQUEST, $"Maximum offset count of {maximumOffset}.");
                     offset = offsetTemp;
                 }
             }
 
-            List<Event> events = new List<Event>();
+            var events = new List<Object>();
 
             using (var db = new ChengetaContext())
             {
-                events.AddRange((from evt in db.events orderby evt.Date descending select evt).Skip(offset).Take(rows).ToList());
+                events.AddRange(
+                    (from evt in db.events
+                     orderby evt.Date descending
+                     select new
+                     {
+                         Id = evt.Id,
+                         NodeId = evt.NodeId,
+                         Date = (long)((DateTime)(evt.Date)).ToUniversalTime().Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds,
+                         Latitude = evt.Latitude,
+                         Longitude = evt.Longitude,
+                         SoundLabel = evt.SoundLabel,
+                         Probability = evt.Probability,
+                         SoundURL = evt.SoundURL
+                     }
+                     ).Skip(offset).Take(rows).ToList());
             }
 
-            var res = new {
+            var res = new
+            {
                 success = true,
                 message = $"Retrieved {events.Count} rows.",
                 data = events
