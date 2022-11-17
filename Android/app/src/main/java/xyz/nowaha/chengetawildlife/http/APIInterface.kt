1f8b0e53b92d3e5995a99cbf352b037b0dff9ff7
Nowaha <noahverkaik@gmail.com>
2022-11-17 16:23:15 +0100

    Method to make account creation request

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/http/APIInterface.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/http/APIInterface.kt
@@ -1,8 +1,12 @@
 package xyz.nowaha.chengetawildlife.http
 
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
+import xyz.nowaha.chengetawildlife.pojo.AccountCreationResponse
 import xyz.nowaha.chengetawildlife.pojo.Event
 import xyz.nowaha.chengetawildlife.pojo.EventListResponse
 import xyz.nowaha.chengetawildlife.pojo.LoginResponse
@@ -14,5 +18,9 @@ interface APIInterface {
 
     @GET("/user/login?")
     fun attemptLogin(@Query("username") username: String, @Query("password") password: String) : Call<LoginResponse>
+
+    @FormUrlEncoded
+    @POST("/accounts/create?")
+    fun createAccount(@Field("username") username: String, @Field("password") password: String, @Field("role") role: Int) : Call<AccountCreationResponse>
 
 }
