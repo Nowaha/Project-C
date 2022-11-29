ed9ef94b9a9817d4bd9fa68eda39c747838a3d07
Nowaha <noahverkaik@gmail.com>
2022-11-29 22:51:43 +0100

    Enabled & switched to view binding

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/DeleteAccountFragment.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/DeleteAccountFragment.kt
@@ -1,8 +1,31 @@
 package xyz.nowaha.chengetawildlife
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import xyz.nowaha.chengetawildlife.databinding.FragmentAccountDeleteBinding
 
 class DeleteAccountFragment : Fragment() {
-    val viewModel: DeleteAccountViewModel by viewModels();
+    val viewModel: DeleteAccountViewModel by viewModels()
+
+    private var _binding: FragmentAccountDeleteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAccountDeleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
