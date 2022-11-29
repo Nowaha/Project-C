4df5440d7872afa8e17555b7955630e9bc4283b7
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2022-11-30 00:09:11 +0100

    Fragment and Viewmodel
    Listens to click, call the delete request method, print result to console for now

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/DeleteAccountFragment.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/DeleteAccountFragment.kt
@@ -1,8 +1,72 @@
 package xyz.nowaha.chengetawildlife
 
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DeleteAccountFragment : Fragment() {
-    val viewModel: DeleteAccountViewModel by viewModels();
+    private val viewModel: DeleteAccountViewModel by viewModels();
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val usernameInput = view.findViewById<TextInputEditText>(R.id.usernameDeleteTextInputEditText)
+        val usernameInputLayout = view.findViewById<TextInputLayout>(R.id.usernameDeleteTextInputLayout)
+
+        usernameInput.setText(viewModel.usernameInput.value)
+        usernameInput.addTextChangedListener {
+            usernameInputLayout.error = null
+            viewModel.usernameInput.postValue(it.toString())
+        }
+        val deleteAccountButton = view.findViewById<Button>(R.id.delete_account_Button)
+        deleteAccountButton.setOnClickListener {
+            if (viewModel.deleteAccountState.value !is DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput) return@setOnClickListener
+            usernameInputLayout.error = null
+            var validAccountDetails = true
+            if (usernameInput.text.toString().isBlank()) {
+                usernameInputLayout.error = "Please enter a username."
+                validAccountDetails = false
+            }
+            if (!validAccountDetails) return@setOnClickListener
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.deleteAccount()
+            }
+            viewModel.deleteAccountState.observe(viewLifecycleOwner) {
+                when (it) {
+                    is DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput -> {
+                        if (it.error != null) {
+                            usernameInputLayout.error = when (it.error) {
+                                DeleteAccountViewModel.DeleteAccountState.DeleteAccountErrorType.CONNECTION_FAILURE -> "Connection Failed"
+                                DeleteAccountViewModel.DeleteAccountState.DeleteAccountErrorType.UNKNOWN_ERROR -> "Something went wrong"
+                                DeleteAccountViewModel.DeleteAccountState.DeleteAccountErrorType.USERNAME_IN_USE -> "Account ${viewModel.usernameInput.value} doesn't exists"
+                            }
+                        }
+                    }
+                    is DeleteAccountViewModel.DeleteAccountState.Loading -> {
+
+                    }
+
+                    is DeleteAccountViewModel.DeleteAccountState.AccountDeleted -> {
+                        Toast.makeText(
+                            requireContext(),
+                            "Account ${viewModel.usernameInput.value} successfully deleted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+
+    }
 }
--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/DeleteAccountViewModel.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/DeleteAccountViewModel.kt
@@ -1,6 +1,69 @@
 package xyz.nowaha.chengetawildlife
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import xyz.nowaha.chengetawildlife.http.APIClient
+
+import xyz.nowaha.chengetawildlife.pojo.AccountDeleteRequest
+import xyz.nowaha.chengetawildlife.pojo.AccountDeleteResponse
 
 class DeleteAccountViewModel: ViewModel() {
+
+    val usernameInput = MutableLiveData("")
+
+    val deleteAccountState =
+        MutableLiveData<DeleteAccountViewModel.DeleteAccountState>(DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput(null))
+
+    suspend fun deleteAccount() = withContext(Dispatchers.IO) {
+        if (deleteAccountState.value !is DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput) return@withContext
+        deleteAccountState.postValue(DeleteAccountViewModel.DeleteAccountState.Loading)
+
+        delay(500)
+
+        var deleteAccountResponse: Response<AccountDeleteResponse>? = null
+        try {
+            deleteAccountResponse = APIClient.getAPIInterface().deleteAccount(
+                AccountDeleteRequest(
+                    usernameInput.value.toString()
+                )
+            ).execute()
+        } catch (_: Exception) {
+
+        }
+        if (deleteAccountResponse?.body() == null && (deleteAccountResponse?.errorBody() == null || deleteAccountResponse.code() == 404)) {
+            deleteAccountState.postValue(
+                DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput(
+                    DeleteAccountViewModel.DeleteAccountState.DeleteAccountErrorType.CONNECTION_FAILURE))
+            return@withContext
+        }
+
+        if (deleteAccountResponse.errorBody() != null) {
+            if (deleteAccountResponse.code() == 400)
+                deleteAccountState.postValue(
+                    DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput(
+                        DeleteAccountViewModel.DeleteAccountState.DeleteAccountErrorType.USERNAME_IN_USE))
+            else
+                deleteAccountState.postValue(
+                    DeleteAccountViewModel.DeleteAccountState.WaitingForUserInput(
+                        DeleteAccountViewModel.DeleteAccountState.DeleteAccountErrorType.UNKNOWN_ERROR))
+
+            return@withContext
+        }
+
+        deleteAccountState.postValue(DeleteAccountViewModel.DeleteAccountState.AccountDeleted)
+    }
+    sealed class DeleteAccountState {
+        data class WaitingForUserInput(val error: DeleteAccountErrorType?) : DeleteAccountState()
+        object Loading : DeleteAccountState()
+        object AccountDeleted : DeleteAccountState()
+
+        enum class DeleteAccountErrorType {
+            CONNECTION_FAILURE, USERNAME_IN_USE, UNKNOWN_ERROR
+        }
+    }
+
 }
