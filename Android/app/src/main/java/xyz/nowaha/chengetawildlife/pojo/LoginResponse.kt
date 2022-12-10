12c1c2ec362b778135525267ecafd2438b4e6e58
Nowaha <noahverkaik@gmail.com>
2022-12-10 16:28:14 +0100

    Removed unnecessary space

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/pojo/LoginResponse.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/pojo/LoginResponse.kt
@@ -1,3 +1,8 @@
 package xyz.nowaha.chengetawildlife.pojo
 
-data class LoginResponse(val success: Boolean, val message: String, val sessionKey: String?, val isAdmin : Boolean)
+data class LoginResponse(
+    val success: Boolean,
+    val message: String,
+    val sessionKey: String?,
+    val isAdmin: Boolean
+)
