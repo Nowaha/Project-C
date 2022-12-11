25ec2495ff2839602215f102a86a58c432646aec
SalaheddineZ <99356984+SalaheddineZ@users.noreply.github.com>
2022-12-12 00:36:03 +0100

    Moved changes to a single file

--- Backend/requests/LoginRequestHandler.cs
+++ Backend/requests/LoginRequestHandler.cs
@@ -17,24 +17,6 @@ namespace ChengetaBackend
             string bodyRaw
         )
         {
-            if (!(Program.sessionManager.SessionDictionary.ContainsKey(session)))
-            {
-                return new Response(
-                    Code.SUCCESS,
-                    Message.SUCCESS,
-                    Encoding.UTF8.GetBytes(
-                        JsonSerializer.Serialize(
-                            new { success = true, message = "Session is invalid" }
-                        )
-                    )
-                );
-            }
-            if (Program.sessionManager.SessionDictionary.ContainsKey(session))
-                return Response.generateBasicError(
-                    Code.BAD_REQUEST,
-                    Message.BAD_REQUEST,
-                    "Error: ALREADY_LOGGED_IN"
-                );
             if ((!args.ContainsKey("username") || args["username"] == null))
                 return Response.generateBasicError(
                     Code.BAD_REQUEST,
@@ -62,38 +44,15 @@ namespace ChengetaBackend
             {
                 Account.AccountType? res = db.accounts
                     .Where(x => x.Username == username)
-                    .Select(x => x.Role)
-                    .FirstOrDefault();
+                    .Select(x => x.Role).FirstOrDefault();
 
                 if (res.HasValue)
                 {
                     type = res.Value;
                 }
-                if (type == Account.AccountType.ADMIN)
-                {
-                    return new Response(
-                        Code.SUCCESS,
-                        Message.SUCCESS,
-                        Encoding.UTF8.GetBytes(
-                            JsonSerializer.Serialize(
-                                new
-                                {
-                                    success = true,
-                                    valid = true,
-                                    message = "Session is valid",
-                                    isAdmin = type,
-                                    username = Program.sessionManager.SessionDictionary[session]
-                                }
-                            )
-                        )
-                    );
-                }
             }
 
-            SessionManager.AuthResult authResult = Program.sessionManager.Authenticate(
-                username,
-                password
-            );
+            SessionManager.AuthResult authResult = Program.sessionManager.Authenticate(username, password);
             if (authResult.resultCode == SessionManager.ResultCode.SUCCESS)
             {
                 return new Response(
