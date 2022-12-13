4eb1a79127d0fb7f8f77311c2e0fadc52db9c3ab
Nowaha <noahverkaik@gmail.com>
2022-12-14 00:08:38 +0100

    Actually update viewmodel's text value

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/ui/AccountOverviewFragment.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/ui/AccountOverviewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -29,6 +30,11 @@ class AccountOverviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.usernameTextInputEditText.setText(viewModel.usernameInput.value ?: "")
+        binding.usernameTextInputEditText.addTextChangedListener {
+            viewModel.usernameInput.postValue(it.toString())
+        }
 
         binding.usernameTextInputLayout.setEndIconOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
