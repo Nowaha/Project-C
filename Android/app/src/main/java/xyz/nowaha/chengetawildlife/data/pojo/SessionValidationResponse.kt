7147d0c4587b4ad39ddc86dcc93c4ddb1ffe032c
Nowaha <noahverkaik@gmail.com>
2022-12-13 22:55:01 +0100

    Made optional values optional

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/SessionValidationResponse.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/SessionValidationResponse.kt
@@ -3,8 +3,8 @@ package xyz.nowaha.chengetawildlife.data.pojo
 data class SessionValidationResponse(
     val success: Boolean,
     val message: String,
-    val isAdmin: Boolean,
-    val valid: Boolean,
-    val username: String
+    val isAdmin: Boolean?,
+    val valid: Boolean?,
+    val username: String?
 )
 
