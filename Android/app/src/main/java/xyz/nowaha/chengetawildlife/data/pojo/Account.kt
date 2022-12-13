2fde857ba14a0c0044c8ef600703bd27d2404a83
julian <julian-_-vink@hotmail.com>
2022-12-13 13:03:36 +0100

    [Feature] GetAccounts
    Create a GET request method (in APIInterface) that can also read the response (through Retrofit) in the front-end.

---
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/Account.kt
@@ -0,0 +1,22 @@
+package xyz.nowaha.chengetawildlife.data.pojo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+@Entity
+data class Account(
+    @PrimaryKey
+    @SerializedName("Id")
+    var id: Int,
+    @SerializedName("Username")
+    var username: String,
+    @SerializedName("Role")
+    var role: String,
+    @SerializedName("CreationDate")
+    var creationDate: Long,
+
+    // Local caching
+    var cacheExpiry: Long?
+)
