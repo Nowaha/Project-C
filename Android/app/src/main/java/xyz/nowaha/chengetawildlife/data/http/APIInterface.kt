64609773a576058834812b069a251d88e38e13c3
Nowaha <noahverkaik@gmail.com>
2022-12-13 23:15:53 +0100

    Added ability to search by username

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/http/APIInterface.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/http/APIInterface.kt
@@ -10,22 +10,38 @@ import xyz.nowaha.chengetawildlife.data.pojo.*
 interface APIInterface {
 
     @GET("/events/latest?")
-    fun getLatestEvents(@Query("rows") rows: Int = 100, @Query("offset") offset: Int = 0) : Call<EventListResponse>
+    fun getLatestEvents(
+        @Query("rows") rows: Int = 100,
+        @Query("offset") offset: Int = 0
+    ): Call<EventListResponse>
 
     @GET("/user/login?")
-    fun attemptLogin(@Query("username") username: String, @Query("password") password: String) : Call<LoginResponse>
+    fun attemptLogin(
+        @Query("username") username: String,
+        @Query("password") password: String
+    ): Call<LoginResponse>
 
     @GET("/accounts/session/validate")
-    fun validateSession() : Call<SessionValidationResponse>
-    
+    fun validateSession(): Call<SessionValidationResponse>
+
     @GET("/accounts/view?")
-    fun getAccountList(@Query("rows") rows: Int = 100, @Query("offset") offset: Int = 0) : Call<AccountListResponse>
+    fun getAccountList(
+        @Query("rows") rows: Int = 100,
+        @Query("offset") offset: Int = 0
+    ): Call<AccountListResponse>
+
+    @GET("/accounts/view?")
+    fun getAccountListByUsername(
+        @Query("rows") rows: Int = 100,
+        @Query("offset") offset: Int = 0,
+        @Query("username") username: String
+    ): Call<AccountListResponse>
 
     @POST("/accounts/create")
-    fun attemptCreateAccount(@Body request: AccountCreationRequest) : Call<AccountCreationResponse>
+    fun attemptCreateAccount(@Body request: AccountCreationRequest): Call<AccountCreationResponse>
 
     @POST("/accounts/delete")
-    fun deleteAccount(@Body request: AccountDeleteRequest) : Call<AccountDeleteResponse>
+    fun deleteAccount(@Body request: AccountDeleteRequest): Call<AccountDeleteResponse>
 
     @POST("/accounts/edit")
     fun attemptEditAccount(@Body request: AccountEditRequest) : Call<AccountEditResponse>
