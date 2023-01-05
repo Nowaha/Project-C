c9f1b9f837e2a74c71aed8ceaa0f4d044ab5493e
Nowaha <noahverkaik@gmail.com>
2023-01-05 23:41:47 +0100

    Update EventStatus.kt

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/EventStatus.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/EventStatus.kt
@@ -2,15 +2,18 @@ package xyz.nowaha.chengetawildlife.data.pojo
 
 object EventStatus {
 
-    private val statusStrings = hashMapOf(
+    private val statusStrings = linkedMapOf(
         0 to "New",
         5 to "Underway",
         10 to "False alarm",
         25 to "Handled"
     )
 
-    fun of(index: Int): String? {
-        return statusStrings[index]
-    }
+    fun of(index: Int): String? = statusStrings[index]
+    fun numberOf(value: String): Int? = statusStrings.filter { it.value.lowercase() == value.lowercase() }.map { it.key }.firstOrNull()
+    fun getValues() = statusStrings.values
+    fun getCopy() = HashMap(statusStrings)
+    fun indexOfNumber(num: Int) = statusStrings.keys.indexOf(num)
+    fun numberAtIndex(num: Int) = statusStrings.keys.toTypedArray()[num]
 
 }
