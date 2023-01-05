03a5c89e5b9145672b0d17c01a08df8bec0597ff
Nowaha <noahverkaik@gmail.com>
2023-01-05 03:48:29 +0100

    Issue/bug in capitalization: data was not being passed properly

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/AccountCreationRequest.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/pojo/AccountCreationRequest.kt
@@ -1,3 +1,9 @@
 package xyz.nowaha.chengetawildlife.data.pojo
 
-data class AccountCreationRequest(val username: String, val password: String, val role: Int, val FirstName: String, val LastName: String)
+data class AccountCreationRequest(
+    val username: String,
+    val password: String,
+    val role: Int,
+    val firstName: String,
+    val lastName: String
+)
