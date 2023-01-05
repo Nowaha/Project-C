994c19cd5c3bc7a6037b51fa3df74002f163db7a
Matthijs Vos <matthijsdell1@outlook.com>
2023-01-05 21:34:08 +0100

    Updated Event Status Class
    Fixed class error

--- Backend/requests/EventStatusRequestHandler.cs
+++ Backend/requests/EventStatusRequestHandler.cs
@@ -12,70 +12,70 @@ namespace ChengetaBackend
         public string Path => "/events/status";
 
         public Method Method => Method.POST;
-    }
-
-
-    public Response HandleRequest(string session, Dictionary<string, string> args, string bodyRaw)
-    {
-
-        if (!Program.sessionManager.SessionDictionary.ContainsKey(session))
-        {
-            return Response.generateBasicError(Code.UNAUTHORIZED, Message.UNAUTHORIZED, "Invalid session");
-        }
 
 
 
-
-        EventStatusEditRequest request;
-
-        try
-        {
-            request = JsonSerializer.Deserialize<EventStatusEditRequest>(bodyRaw);
-        }
-        catch (Exception)
-        {
-            return Response.generateBasicError(Code.BAD_REQUEST, Message.BAD_REQUEST, "Invalid request structure.");
-        }
-
-
-        int eventId = request.eventId;
-        int status = request.status;
-
-
-
-
-        using (var db = new ChengetaContext())
+        public Response HandleRequest(string session, Dictionary<string, string> args, string bodyRaw)
         {
 
-            var currentEvent = db.events.Where(currEvent => currEvent.Id == eventId).FirstOrDefault();
-            if (currentEvent == null)
+            if (!Program.sessionManager.SessionDictionary.ContainsKey(session))
             {
-                return Response.generateBasicError(
-                        Code.BAD_REQUEST,
-                        Message.BAD_REQUEST,
-                        "Event doesn't exist.");
+                return Response.generateBasicError(Code.UNAUTHORIZED, Message.UNAUTHORIZED, "Invalid session");
             }
-            currentEvent.Status = status;
-            db.SaveChanges();
 
 
-            return new Response(
-                Code.SUCCESS,
-                Message.SUCCESS,
-                Encoding.UTF8.GetBytes(
-                    JsonSerializer.Serialize(
-                        new
-                        {
-                            success = true,
-                            message = $"Event status of event {eventId} updated successfully"
-                        }
 
-                ))
-            );
+
+            EventStatusEditRequest request;
+
+            try
+            {
+                request = JsonSerializer.Deserialize<EventStatusEditRequest>(bodyRaw);
+            }
+            catch (Exception)
+            {
+                return Response.generateBasicError(Code.BAD_REQUEST, Message.BAD_REQUEST, "Invalid request structure.");
+            }
+
+
+            int eventId = request.eventId;
+            int status = request.status;
+
+
+
+
+            using (var db = new ChengetaContext())
+            {
+
+                var currentEvent = db.events.Where(currEvent => currEvent.Id == eventId).FirstOrDefault();
+                if (currentEvent == null)
+                {
+                    return Response.generateBasicError(
+                            Code.BAD_REQUEST,
+                            Message.BAD_REQUEST,
+                            "Event doesn't exist.");
+                }
+                currentEvent.Status = status;
+                db.SaveChanges();
+
+
+                return new Response(
+                    Code.SUCCESS,
+                    Message.SUCCESS,
+                    Encoding.UTF8.GetBytes(
+                        JsonSerializer.Serialize(
+                            new
+                            {
+                                success = true,
+                                message = $"Event status of event {eventId} updated successfully"
+                            }
+
+                    ))
+                );
+            }
+
+
         }
-
-
     }
-
 
 }
