7cea8e22b90c3b68b82afdb7a43acd04de567dea
Nowaha <noahverkaik@gmail.com>
2023-01-05 15:41:01 +0100

    Upped Room db version

--- Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/AppDatabase.kt
+++ Android/app/src/main/java/xyz/nowaha/chengetawildlife/data/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import xyz.nowaha.chengetawildlife.data.pojo.Event
 
-@Database(entities = [Session::class, Event::class], version = 2)
+@Database(entities = [Session::class, Event::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun sessionDao(): SessionDao
